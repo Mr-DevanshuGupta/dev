9ecaf3aa78d26ee58500d994afbc5c011af521b5
Devanshu Gupta <devanshu.kissu.0000@gmail.com>
2023-04-18 21:12:07 +0530

    navbar completed

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {motion} from 'framer-motion';
+import {styles} from './styles';
+import {ComputersCanvas} from './canvas';
+
+const Hero = () => {
+  return (
+    <section className='relative w-full h-screen mx-auto'>Hero</section>
+  )
+}
+
+export default Hero
