52a60ed712f8fffb327698ce2180146f0077149d
Devanshu Gupta <devanshu.kissu.0000@gmail.com>
2023-04-28 12:33:16 +0530

    almost

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,10 @@ const App = () => {
         <Hero/>
       </div>
       <About/>
-      <Experience/>
+      {/* <Experience/> */}
       <Tech/>
       <Works/>
-      <Feedbacks/>
+      {/* <Feedbacks/> */}
       <div className="relative z-0">
         <Contact/>
         <StarsCanvas/>
